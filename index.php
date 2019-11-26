9d698336ec127bd26f2c2e62751c196409992c70
Alessandro Marchetti <marchettiale01@gmail.com>
2019-11-26 13:22:48 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,120 @@
+<html>
+
+<head>
+    <title>CODICE FISCALE</title>
+</head>
+
+<body>
+    <form action="result.php" method="POST">
+        <div id="principale">
+            <table width="50%">
+                <tr>
+                    <td>
+                        <p>NOME:</p>
+                    </td>
+                    <td>
+                        <input type="text" name="nome">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>COGNOME:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <input type="text" name="cognome">
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>GIORNO:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <select name="giorno">
+                            <?php
+                            for ($i = 0; $i < 32; $i++) {
+                                echo "<option value='$i'>$i</option>";
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>MESE:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <select name="mese">
+                            <option value="01">GENNAIO</option>
+                            <option value="02">FEBBRAIO</option>
+                            <option value="03">MARZO</option>
+                            <option value="04">APRILE</option>
+                            <option value="05">MAGGIO</option>
+                            <option value="06">GIUGNO</option>
+                            <option value="07">LUGLIO</option>
+                            <option value="08">AGOSTO</option>
+                            <option value="09">SETTEMBRE</option>
+                            <option value="10">OTTOBRE</option>
+                            <option value="11">NOVEMBRE</option>
+                            <option value="12">DICEMBRE</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>ANNO:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <select name="anno">
+                            <?php
+                            for ($i = 1900; $i < 2020; $i++) {
+                                echo "<option value='$i'>$i</option>";
+                            }
+                            ?>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>SESSO:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <select name="sesso">
+                            <option value="M">MASCHIO</option>
+                            <option value="F">FEMMINA</option>
+                            <option value="C">ALTRO</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <p>COMUNE:</p>
+                    </td>
+                    <td>
+                        <br>
+                        <select name="comune">
+                            <option value="VICENZA">VICENZA</option>
+                            <option value="BASSANO DEL GRAPPA">BASSANO</option>
+                            <option value="VALDAGNO">VALDAGNO</option>
+                            <option value="ARZIGNANO">ARZIGANO</option>
+                        </select>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <button type="submit">CALCOLA</button>
+                    </td>
+                </tr>
+            </table>
+            <div id=" risult">
+                <p id="out"></p>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ result.php
@@ -0,0 +1,372 @@
+<?php
+
+$d = $_POST;
+
+echo CalcolaCodice($d["cognome"], $d["nome"], $d["giorno"], $d["mese"], $d["anno"], $d["sesso"], $d["comune"]);
+
+function CalcolaCodice($cognome, $nome, $giorno, $mese, $anno, $sesso, $comune)
+{
+    $risultato = "";
+
+    $cognomeC = CalcolaCognome($cognome);
+    $risultato .= $cognomeC;
+
+
+    $nomeC = CalcolaNome($nome);
+    $risultato .= $nomeC;
+
+
+    $risData = CalcolaData($giorno, $mese, $anno, $sesso);
+    $risultato .= $risData;
+
+    $comuneC = Catastali($comune);
+    $risultato .= $comuneC;
+    $carattere = CalcolaCarattere($risultato);
+    $risultato .= $carattere;
+
+
+    return "Il tuo codice fiscale è: $risultato";
+}
+function CalcolaCarattere($ris)
+{
+
+    $i = 0;
+    $pari = "";
+    $dispari = "";
+
+    for ($i = 0; $i < 15; $i++) {
+        $let = $ris{
+            $i};
+        if ($i % 2 != 0) {
+            $pari .= $let;
+        } else {
+            $dispari .= $let;
+        }
+    }
+
+    $numPari = CalcolaPari($pari);
+    $numDispari = CalcolaDispari($dispari);
+
+    $resto = ($numDispari + $numPari) % 26;
+    $finale = CalcolaResto($resto);
+    return $finale;
+}
+function CalcolaResto($resto)
+{
+    $res = [
+        "0" => "A",
+        "1" => "B",
+        "2" => "C",
+        "3" => "D",
+        "4" => "E",
+        "5" => "F",
+        "6" => "G",
+        "7" => "H",
+        "8" => "I",
+        "9" => "J",
+        "10" => "K",
+        "11" => "L",
+        "12" => "M",
+        "13" => "N",
+        "14" => "O",
+        "15" => "P",
+        "16" => "Q",
+        "17" => "R",
+        "18" => "S",
+        "19" => "T",
+        "20" => "U",
+        "21" => "V",
+        "22" => "W",
+        "23" => "X",
+        "24" => "Y",
+        "25" => "Z"
+    ];
+
+    $a = $res[$resto];
+    return $a;
+}
+function CalcolaPari($pari)
+{
+    $con = [
+
+        "0" => 0,
+        "1" => 1,
+        "2" => 2,
+        "3" => 3,
+        "4" => 4,
+        "5" => 5,
+        "6" => 6,
+        "7" => 7,
+        "8" => 8,
+        "9" => 9,
+        "A" => 0,
+        "B" => 1,
+        "C" => 2,
+        "D" => 3,
+        "E" => 4,
+        "F" => 5,
+        "G" => 6,
+        "H" => 7,
+        "I" => 8,
+        "J" => 9,
+        "K" => 10,
+        "L" => 11,
+        "M" => 12,
+        "N" => 13,
+        "O" => 14,
+        "P" => 15,
+        "Q" => 16,
+        "R" => 17,
+        "S" => 18,
+        "T" => 19,
+        "U" => 20,
+        "V" => 21,
+        "W" => 22,
+        "X" => 23,
+        "Y" => 24,
+        "Z" => 25
+    ];
+
+    $i = 0;
+    $pariN = 0;
+
+    for ($i = 0; $i < strlen($pari); $i++) {
+        $pariN += $con[$pari{
+            $i}];
+    }
+
+    return $pariN;
+}
+function CalcolaDispari($dispari)
+{
+    $con = [
+
+        "0" => 1,
+        "1" => 0,
+        "2" => 5,
+        "3" => 7,
+        "4" => 9,
+        "5" => 13,
+        "6" => 15,
+        "7" => 17,
+        "8" => 19,
+        "9" => 21,
+        "A" => 1,
+        "B" => 0,
+        "C" => 5,
+        "D" => 7,
+        "E" => 9,
+        "F" => 13,
+        "G" => 15,
+        "H" => 17,
+        "I" => 19,
+        "J" => 21,
+        "K" => 2,
+        "L" => 4,
+        "M" => 18,
+        "N" => 20,
+        "O" => 11,
+        "P" => 3,
+        "Q" => 6,
+        "R" => 8,
+        "S" => 12,
+        "T" => 14,
+        "U" => 16,
+        "V" => 10,
+        "W" => 22,
+        "X" => 25,
+        "Y" => 24,
+        "Z" => 23
+    ];
+
+    $i = 0;
+    $dispariN = 0;
+
+    for ($i = 0; $i < strlen($dispari); $i++) {
+        $dispariN += $con[$dispari{
+            $i}];
+    }
+
+    return $dispariN;
+}
+function CalcolaData($gio, $mes, $ann, $s)
+{
+
+    $risData = "";
+    $risData .= $ann{
+        2};
+    $risData .= $ann{
+        3};
+
+    $risData .= CalcolaMese($mes);
+
+    $giorno = "";
+
+    if ($s == "M") {
+        if ($gio < 10) {
+            $giorno = "0" + $gio;
+        } else {
+            $giorno = $gio;
+        }
+    } else {
+        $giorno = intval($gio) + 40;
+    }
+
+    $risData .= $giorno;
+
+    return $risData;
+}
+function CalcolaMese($mes)
+{
+    $mesi = [
+        "01" => "A",
+        "02" => "B",
+        "03" => "C",
+        "04" => "D",
+        "05" => "E",
+        "06" => "H",
+        "07" => "L",
+        "08" => "M",
+        "09" => "P",
+        "10" => "R",
+        "11" => "S",
+        "12" => "T"
+    ];
+
+    $a = $mesi[$mes];
+
+    return $a;
+}
+function Catastali($n)
+{
+    $comune = [
+        'VICENZA' => "L840",
+        'BASSANO DEL GRAPPA' => "A703",
+        'VALDAGNO' => "L551",
+        'ARZIGNANO' => "A459"
+    ];
+
+    $cumRis = $comune[$n];
+
+    return $cumRis;
+}
+function CalcolaCognome($cognome)
+{
+
+    $cognome = strtoupper($cognome);
+    $ris = "";
+    $tot = 0;
+    for ($i = 0; ($i < strlen($cognome)) && ($tot < 3); $i++) {
+
+        $lettera = $cognome{
+            $i};
+        if (!Vocale($lettera)) {
+            if ($lettera != $cognome{
+                $i - 1}) {
+                $ris .= $lettera;
+                $tot += 1;
+            }
+        }
+    }
+
+    if (strlen($ris) < 3) {
+        for ($i = 0; ($i < strlen($cognome)) && ($tot < 3); $i++) {
+            $lettera = $cognome{
+                $i};
+
+            if (Vocale($lettera)) {
+                $ris .= $lettera;
+                $tot++;
+            }
+        }
+    } else {
+        return $ris;
+    }
+    for (; strlen($ris) < 3;) {
+        $ris .= "X";
+        $tot++;
+    }
+    return $ris;
+}
+function CalcolaNome($nome)
+{
+
+    $nome = strtoupper($nome);
+
+    $ris = "";
+    $i = 0;
+    $tot = 0;
+
+    $ris1 = "";
+    $tot1 = 0;
+
+    for ($i = 0; $i < strlen($nome); $i++) {
+
+        $lettera = $nome{
+            $i};
+        if (!Vocale($lettera)) {
+            $ris1 .= $lettera;
+            $tot1++;
+        }
+    }
+
+    if ($tot1 > 3) {
+        $ris = PiuConsonanti($ris1);
+        return $ris;
+    } else if ($tot1 == 3) {
+        $ris = $ris1;
+        return $ris;
+    }
+
+    $ris = $ris1;
+
+    if (strlen($ris) < 3) {
+        for ($i = 0; $i < strlen($nome); $i++) {
+            $lettera = $nome{
+                $i};
+
+            if (Vocale($lettera)) {
+                $ris .= $lettera;
+                $tot++;
+            }
+        }
+    } else {
+        return $ris;
+    }
+    for (; strlen($ris) < 3;) {
+        $ris .= "X";
+        $tot++;
+    }
+    return $ris;
+}
+function PiuConsonanti($cons)
+{
+    $ris = "";
+    $ris = $cons{
+        0} . $cons{
+        2} . $cons{
+        3};
+    return $ris;
+}
+function Vocale($let)
+{
+    if (($let == "A") || ($let == "E") || ($let == "I") || ($let == "O") || ($let == "U")) {
+        return true;
+    } else {
+        return false;
+    }
+}
+?>
+
+<html>
+
+<head>
+    <title>Result</title>
+</head>
+
+<body>
+    <br>
+    <a href="index.php"> TORNA ALLA HOME </a>
+</body>
+
+</html>
